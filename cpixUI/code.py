432982a48164ed3150128aa1de210c65e06d700b
Tom Hoffman <tom.hoffman@gmail.com>
2025-01-07 19:24:42 -0500

    Added .add()

--- cpixUI/code.py
+++ cpixUI/code.py
@@ -7,7 +7,9 @@ import board
 import neopixel
 
 lStart = 4
+lEnd   = 0
 rStart = 5
+rEnd   = 9
 
 class HalfBar(object):
     # HalfBar :: (pixels :: list<tuple><int>, 
@@ -16,11 +18,17 @@ class HalfBar(object):
         self.pixels = pixels
         self.length = length % 6
         self.color = color
+        
+    def add(self):
+        self.length = (self.length + 1) % 6
+
+    def subtract(self):
+        self.length = (self.length - 1) % 6
 
 class LeftBar(HalfBar):
     # this starts at pixel 4 and goes clockwise to pixel 0
     def clear(self):
-        for i in range(0, lStart + 1):
+        for i in range(lEnd, lStart + 1):
             self.pixels[i] = (0, 0, 0)
 
     def draw(self):
@@ -33,7 +41,7 @@ class LeftBar(HalfBar):
 class RightBar(HalfBar):
     # this starts at pixel 5 and goes anticlockwise to pixel 9
     def clear(self):
-        for i in range(rStart, 10):
+        for i in range(rStart, rEnd + 1):
             self.pixels[i] = (0, 0, 0)
     def draw(self):
         self.clear()
@@ -42,10 +50,14 @@ class RightBar(HalfBar):
             self.pixels[i] = self.color
         self.pixels.show()
 
-p = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.2, auto_write=False)
+p = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.2, 
+    auto_write=False)
 l = LeftBar(p, 3, (32, 0, 0))
+l.add()
 l.draw()
-r = RightBar(p, 3, (0, 32, 0))
+r = RightBar(p, 0, (0, 32, 0))
+r.subtract()
+r.subtract()
 r.draw()
 print(gc.mem_free())
 while 1:
