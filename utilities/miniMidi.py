da461606866f132f942a98895619e6274339aeec
Tom Hoffman <tom.hoffman@gmail.com>
2025-10-21 14:37:16 -0400

    Refactor MiniMidi for better MIDI message handling
    Refactor MiniMidi class to improve message processing and channel handling.

--- utilities/miniMidi.py
+++ utilities/miniMidi.py
@@ -5,29 +5,7 @@
 # WIP description:
 # MiniMidi will be a minimalist MIDI library aimed at highly memory constrained 
 # CircuitPython devices, specifically the Adafruit Circuit Playground Express.
-
-# Supported messages:
-# Note On
-# Note Off
-# Clock
-# Start
-# Stop
-# Continue
-
-# Modes:
-# Playing: plays notes on and off, processes clock, stop changes mode.
-# Stopped: only waits for start or continue
-# * start from beginning
-# * continue from where you left off
-# Skipping: waiting for end of message
-
-
-
-# notes on bytes
-# to make bytes from hex numbers: bytes.fromhex('f8')
-# reverse: aByte.hex()
-# print binary: bin(ord(aByte))
-
+# The program flow is structured around the patterns of MIDI binary encoding.
 
 DEV_MODE = True
 
@@ -38,10 +16,6 @@ if DEV_MODE:
     print("Starting free bytes (after gc) = " + str(gc.mem_free()))
 
 from micropython import const
-
-import board            # helps set up pins, etc. on the board
-import digitalio        # digital (on/off) output to pins, including board LED.
-import neopixel         # controls the RGB LEDs on the board
 
 import usb_midi         # basic MIDI over USB support
 
@@ -54,6 +28,8 @@ if DEV_MODE:
 # Messages we're using:
 _CLOCK = const(0xF80)
 _START = const(0xFA)
+_NOTE_ON_NYBBLE = const(0x1001)
+_NOTE_OFF_NYBBLE = const(0x1000)
 
 # Messages we're specifically NOT using and filtering out.
 _ACTIVE_SENSING = const(0xFE)
@@ -61,67 +37,76 @@ _PITCH_BEND_NYBBLE = const(0b1110)
 
 innie = usb_midi.ports[0]
 outie = usb_midi.ports[1]
-
-def isStatusByte(b):
-    '''This returns true if this is the first part of a status message.'''
-    return b >> 7 & 1
-
-def isSystemMessage(b):
-    return (b >> 4) == 15
-
-def getChannel(b):
-    return b & 0b00001111  
-
-def quickDrop(msg):
-    '''Dropping specific messages we are not using but may flood the input.'''
-    return ((msg == _ACTIVE_SENSING) or (msg >> 4 == _PITCH_BEND_NYBBLE))
-
-def channelDrop(b, channels):
-    # if it is a system level message we won't drop it here
-    if isSystemMessage(b):
-        return False
-    else:
-        # if we aren't listening on any channel drop channel level messages
-        if channels is None:
-            return True
-        else:
-            # return true if this isn't a channel we're listening to
-            return getChannel() not in channels  
+  
 
 class MiniMidi():
-    def __init__(self, in_channels=None, out_channels=None):
-        # add some guards
-        self.in_channels = in_channels    # None or tuple of valid channel numbers
-        self.out_channels = out_channels  # None or tuple of valid channel numbers
-        self.status = "playing"  # "playing", "stopped", "skipping"
-                
-    def get_msg(self):
-        # read one byte
-        raw = (innie.read(1))
-        # if we didn't get an empty byte
+    
+    def __init__(self, in_channel, out_channel):
+        self.in_channel = in_channel & 0b1111    # one 4 bit number
+        self.out_channel = out_channel & 0b1111  # one 4 bit number
+
+    def isSystemMessage(self, d):
+        return d['high'] == 0b1111
+
+    def isMessage(self, n):
+        # n is the high nybble
+        return n & 0b1000
+
+    def processSystemMessage(d):
+        m = d['low']
+        if m == 0b0000:
+            print("System Exclusive")
+        elif m == 0b0001:
+            print("Quarter Frame")
+        elif m == 0b0010:
+            print("Song Position Pointer")
+        elif m == 0b0011:
+            print("Song Select")
+        elif (m == 0b0100) or (m == 0b0101):
+            if DEV_MODE:
+                print("Undefined")
+            return {'msg' : 'Undefined'}
+        elif m == 0b0110:
+            if DEV_MODE:
+                print("Tune Request")
+            return {'msg' : 'Tune Request'}
+
+    def processChannelMessage(d):
+        pass
+
+    def processMessage(self, d):
+        if self.isSystemMessage(d):
+            return self.processSystemMessage(d)
+        else:
+            return self.processChannelMessage(d)
+    
+    def processByte(self, b):
+        # b is a number
+        d = {'high' : b & 0b11110000, 'low' : b & 0b00001111}  
+        if self.isMessage(d['high']):
+            return self.processMessage(d)
+        else:
+            if DEV_MODE:
+                print("Not a message byte.")
+            return None
+    
+    def getByte(self):
+        raw = innie.read()
         if raw != b'':
             # convert the byte type to a number
-            b = ord(raw)
-            # quickly some dropping messages we definitely don't need to use (yet)
-            if quickDrop(b) or channelDrop(b, self.in_channel):  
-                return None
-            if self.status != "stopped":
-                # if we are not stopped, process clock.
-                if b == _CLOCK:
-                    return {'msg' : 'clock'}
-            else:
-                self.status = "playing"
-                if dev_mode:  print("Now playing...")
-                return {'msg' : 'start'}
-            # check channel.
-            
-            # do note on/off
-            # stop/start/continue
-  
-        else:  # if we got an empty byte
-            return None       
+            return ord(raw)     
+        else:
+            return None
+    
+    def get_msg(self):
+        b = self.getByte()
+        if b is None:
+            return None
+        else:
+            # pass on a dict of the high and low nybbles
+            return processMessage(b)    
 
-midi = MiniMidi()
+midi = MiniMidi(0, 15)
 
 if DEV_MODE:
     gc.collect()
@@ -129,4 +114,3 @@ if DEV_MODE:
 
 while True:
     midi.get_msg()
-
