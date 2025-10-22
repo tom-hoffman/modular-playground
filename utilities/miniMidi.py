1eb50ce8dbd095dbdf269520a0c76adc65c71d7e
Tom Hoffman <tom.hoffman@gmail.com>
2025-10-22 19:10:57 -0400

    Partially working with tests.

--- utilities/miniMidi.py
+++ utilities/miniMidi.py
@@ -24,19 +24,19 @@ if DEV_MODE:
 
 innie = usb_midi.ports[0]
 outie = usb_midi.ports[1]
-  
+
 class MiniMidi():
     
     def __init__(self, in_channel, out_channel):
-        self.in_channel = in_channel & 0b1111    # one 4 bit number
-        self.out_channel = out_channel & 0b1111  # one 4 bit number
+        self.in_channel = in_channel & 0b1111    # one 4 bit nybble
+        self.out_channel = out_channel & 0b1111  # one 4 bit nybble
 
     def dropHighBit(self, n):
         # n is a number
         return n & 0b01111111
     
     def isSystemRealTime(self, d):
-        return d['low'] & 0b0100
+        return d['low'] & 0b1000
     
     def isSystemMessage(self, d):
         return d['high'] == 0b1111
@@ -46,10 +46,10 @@ class MiniMidi():
         return n & 0b10000000
 
     def isData(self, n):
-        return not isMessage(n)
+        return not self.isMessage(n)
 
     def isInputChannel(self, n):
-        return m == self.innie
+        return n == self.in_channel
 
     def processSystemExclusive(self, d):
         pass
@@ -68,31 +68,21 @@ class MiniMidi():
     
     def processSystemRealTime(self, d):
         m = d['low'] & 0b0111
-        if DEV_MODE:
-            print(self.REAL_TIME_MESSAGES(m))
-        return {'msg' : self.REAL_TIME_MESSAGES(m)}
+        return {'msg' : self.REAL_TIME_MESSAGES[m]}
             
     def processSystemCommon(self, d):
         m = d['low'] & 0b0111
         if m == 0b000:
-            if DEV_MODE:
-                print("System Exclusive")
             return self.processSystemExclusive(d)
         elif m == 0b001:
-            if DEV_MODE:
-                print("Quarter Frame")
             return self.processQuarterFrame(d)
         elif m == 0b010:
-            if DEV_MODE:
-                print("Song Position Pointer")
+            return self.processSongPosition(d)
         elif m == 0b011:
-            if DEV_MODE:
-                print("Song Select")
-        elif (m == 0b100) or (m == b0101):
+            return self.processSongSelect(d)
+        elif (m == 0b100) or (m == 0b101):
             raise RuntimeError("Undefined system common message.")
         elif m == 0b110:
-            if DEV_MODE:
-                print("Tune Request")
             return {'msg' : 'Tune Request'}
         elif m == 0b111:
             raise RuntimeError("End of SysEx without beginning of SysEx.")
@@ -110,64 +100,61 @@ class MiniMidi():
         v = self.getByte()
         if self.isData(k) and self.isData(v):
             return {'msg' : msg, 
-                    'note' : dropHighBit(k),
-                    'velocity' : dropHighBit(v)}
+                    'note' : self.dropHighBit(k),
+                    'velocity' : self.dropHighBit(v)}
         else:
             raise RuntimeError("Message byte when expecting data byte.")
 
-    def processChannelMessage(d):
+    def processControlChange(self, d):
+        return None
+
+    def processProgramChange(self, d):
+        return None
+    
+    def processChannelPressure(self, d):
+        return None
+    
+    def processPitchBend(self, d):
+        return None
+
+    def processChannelMessage(self, d):
         if self.isInputChannel(d['low']):
-            m = d['high'] & 0b01110000
+            m = d['high'] & 0b0111
             if m == 0b000:
-                if DEV_MODE:
-                    print("Note Off")
                 return self.processNote(d, "Note Off")
             elif m == 0b001:
-                if DEV_MODE:
-                    print("Note On")
                 return self.processNote(d, "Note On")
             elif m == 0b010:
-                if DEV_MODE:
-                    print("Polyphonic Key Pressure")
                 return self.processNote(d, "Polyphonic Key Pressure")
             elif m == 0b011:
-                if DEV_MODE:
-                    print("Control Change")
                 return self.processControlChange(d)
             elif m == 0b100:
-                if DEV_MODE:
-                    print("Program Change")
                 return self.processProgramChange(d)
             elif m == 0b101:
-                if DEV_MODE:
-                    print("Channel Pressure")
                 return self.processChannelPressure(d)
             elif m == 0b110:
-                if DEV_MODE:
-                    print("Pitch Bend")
                 return self.processPitchBend(d)
-            
+            else:
+                raise RuntimeError("Error processing Channel Message")
         else:
-            if DEV_MODE:
-                print("Not the selected input channel.")
             return None         
 
     def processMessage(self, d):
         if self.isSystemMessage(d):
-            return self.processSystemCommon(d)
+            return self.processSystemMessage(d)
         else:
             return self.processChannelMessage(d)
     
     def processMessageByte(self, b):
         # b is a number
-        d = {'high' : b & 0b11110000, 'low' : b & 0b00001111}  
+        d = {'high' : b >> 4, 'low' : b & 0b00001111}  
         if self.isMessage(b):
             return self.processMessage(d)
         else:
             raise RuntimeError("Not a message byte at beginning of message.")
     
     def getByte(self):
-        raw = innie.read()
+        raw = innie.read(1)
         if raw != b'':
             # convert the byte type to a number
             return ord(raw)     
@@ -180,7 +167,7 @@ class MiniMidi():
             return None
         else:
             # pass on a dict of the high and low nybbles
-            return processMessage(b)    
+            return self.processMessageByte(b)    
 
 midi = MiniMidi(0, 15)
 
@@ -189,4 +176,6 @@ if DEV_MODE:
     print("Free bytes after setup = " + str(gc.mem_free()))
 
 while True:
-    midi.get_msg() 
+    m = midi.get_msg() 
+    if m is not None:
+        print(m)
