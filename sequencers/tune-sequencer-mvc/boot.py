49e1675b5c71840dcc954e562157b0606b008990
Tom Hoffman <tom.hoffman@gmail.com>
2025-12-02 19:43:36 -0500

    Add boot.py to set Circuit Playground name
    Sets the name of the Circuit Playground for USB identification.

---
+++ sequencers/tune-sequencer-mvc/boot.py
@@ -0,0 +1,11 @@
+# sets the name of the Circuit Playground as it appears to a PC
+# from https://github.com/todbot/circuitpython-tricks#usb
+
+# customize name here:
+name = "TUNE-SEQ"
+
+import storage
+storage.remount("/", readonly=False)
+m = storage.getmount("/")
+m.label = name
+storage.remount("/", readonly=True)
