89b7ed4516b3dd7f1f071e387572b4237b874a03
Tom Hoffman <tom.hoffman@gmail.com>
2026-03-20 15:06:50 -0400

    Made dirctory for functional euclidian.

---
+++ sequencers/euclidian-functional/config.py
@@ -0,0 +1,32 @@
+# config.py
+
+# Put starting variables here that might need to be changed by the user.
+
+# Give each Circuit Playground a unique name so you don't get confused!
+USB_NAME = "EUCLID0"
+
+# send MIDI messages on:
+# this is the "raw" 0-15 scale
+channel_out = 15
+
+# These are the note values each sequencer will put out.
+# preset for Nord Drum 3p - 60, 62, 64, 65, 67, 69
+NOTE_NUMBERS = (60, 62, 64, 65, 67, 69)
+
+# The note index THIS sequence sends out:
+note = 0
+
+# pulses per quarter note
+PPQN = 24
+
+# duration of analog gate
+# currently must be less than PPQN
+GATE_DURATION = 6
+
+# starting values for the sequencer
+DEFAULT_VELOCITY = 4
+DEFAULT_STEPS = 4
+DEFAULT_TRIGGERS = 1
+DEFAULT_ROTATION = 0
+
+
