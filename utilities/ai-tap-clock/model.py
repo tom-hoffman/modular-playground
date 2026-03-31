7d92dc4a2d955f9e84dbaf7f7721fa8ab75c58f4
Tom Hoffman <tom.hoffman@gmail.com>
2026-03-31 11:35:10 -0400

    Considering and reconsidering some ai experiements.

---
+++ utilities/ai-tap-clock/model.py
@@ -0,0 +1,7 @@
+class ApplicationModel(object):
+    '''This class should contain properties that store the state of the application.'''
+    def __init__(self, changed=True):
+        self.changed = changed
+
+
+
