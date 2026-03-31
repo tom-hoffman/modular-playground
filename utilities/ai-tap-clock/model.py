15bd1fbca172de67f1777082f780765f38e31e56
Tom Hoffman <tom.hoffman@gmail.com>
2026-03-31 11:35:38 -0400

    Deleting stuff...

--- utilities/ai-tap-clock/model.py
+++
@@ -1,7 +0,0 @@
-class ApplicationModel(object):
-    '''This class should contain properties that store the state of the application.'''
-    def __init__(self, changed=True):
-        self.changed = changed
-
-
-
