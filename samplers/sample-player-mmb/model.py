13c805e37ea29ae51dbc66cce052d28d3a72f28a
Tom Hoffman <tom.hoffman@gmail.com>
2026-04-10 08:13:46 -0400

    Needed a self.

--- samplers/sample-player-mmb/model.py
+++ samplers/sample-player-mmb/model.py
@@ -18,7 +18,7 @@ class PlayerModel(object):
     def calculate_sample_index(self) -> int:
         return (self.sample_button_count + self.sample_cc_count) % len(config.BANKS)
 
-    def calculate_bank_index() -> int:
+    def calculate_bank_index(self) -> int:
         return (self.bank_button_count + self.bank_cc_count) % config.SAMPLE_COUNT
 
     def change_sample(self):
